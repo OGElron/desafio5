08f7d4d4d4b0ab1af9f6342ab7c485e7b31cb7a6
Ivan Niello <ivaniello@hotmail.com>
2022-08-16 21:24:17 -0300

    des5

---
+++ handlebars/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const classProductos = require('./api/classProductos.js')
+const app = express();
+const PORT = 8080;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const productosApi = new classProductos();
+
+app.set('views', './views');
+app.set('view engine', 'handlebars'); 
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('index');
+});
+
+app.get('/productos', (req, res) => {
+    const data = productosApi.listarAll();
+    res.render('main', {data: JSON.stringify(data, null, 2)})
+});
+app.post('/productos', (req, res) => {
+    const form = req.body;
+    productosApi.save(form);
+});
+
+/* server listen */
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+
+server.on('error', error => console.log(`Error en servidor ${error}`));
+
